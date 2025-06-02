633d77b77b77b8937b0b3a69a4480cd14af510ec
sudhachandrann <sudhachandran1020@gmail.com>
2025-06-02 06:40:39 +0530

    add movie api and movie card

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,105 @@
-import { SafeAreaView, Text } from "react-native";
+import MovieCard from "@/components/MovieCard";
+import SearchBar from "@/components/SearchBar";
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/usefetch";
+import {  useRouter } from "expo-router";
+import { ActivityIndicator, FlatList, Image, ScrollView, Text, View } from "react-native";
 
 export default function Index() {
+  const router = useRouter();
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+  } = useFetch(() => fetchMovies({ query: "" }));
+
+
   return (
-    <SafeAreaView className="flex-1 items-center justify-center">
-      <Text className="text-blue-800 font-extrabold text-3xl">Welcome To Movix</Text>
-    </SafeAreaView>
+    <View className="flex-1 bg-primary">
+      <Image
+        source={require("../../assets/images/bg.png")}
+        className="absolute w-full z-0"
+        resizeMode="cover"
+      />
+      <ScrollView
+        className="flex-1 px-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
+      >
+        <Image
+          source={require("../../assets/icons/logo.png")}
+          className="w-12 h-10 mt-20 mb-5 mx-auto"
+        />
+         {/* <View className="flex-1 mt-5">
+                             <SearchBar
+              onPress={() => {
+                router.push("/search");
+              }}
+              placeholder="Search for a movie"
+            />
+         </View> */}
+         {moviesLoading  ? (
+          <ActivityIndicator
+            size="large"
+            color="#0000ff"
+            className="mt-10 self-center"
+          />
+        ) : moviesError  ? (
+          <Text>Error: {moviesError?.message }</Text>
+        ) : (
+          <View className="flex-1 mt-5">
+            <SearchBar
+              onPress={() => {
+                router.push("/search");
+              }}
+              placeholder="Search for a movie"
+            />
+
+            {/* {trendingMovies && (
+              <View className="mt-10">
+                <Text className="text-lg text-white font-bold mb-3">
+                  Trending Movies
+                </Text>
+                <FlatList
+                  horizontal
+                  showsHorizontalScrollIndicator={false}
+                  className="mb-4 mt-3"
+                  data={trendingMovies}
+                  contentContainerStyle={{
+                    gap: 26,
+                  }}
+                  renderItem={({ item, index }) => (
+                    <TrendingCard movie={item} index={index} />
+                  )}
+                  keyExtractor={(item) => item.movie_id.toString()}
+                  ItemSeparatorComponent={() => <View className="w-4" />}
+                />
+              </View>
+            )} */}
+
+            <>
+              <Text className="text-lg text-white font-bold mt-5 mb-3">
+                Latest Movies
+              </Text>
+
+              <FlatList
+                data={movies}
+                renderItem={({ item }) => <MovieCard {...item} />}
+                keyExtractor={(item) => item.id.toString()}
+                numColumns={3}
+                columnWrapperStyle={{
+                  justifyContent: "flex-start",
+                  gap: 20,
+                  paddingRight: 5,
+                  marginBottom: 10,
+                }}
+                className="mt-2 pb-32"
+                scrollEnabled={false}
+              />
+            </>
+          </View>
+        )}
+      </ScrollView>
+    </View>
   );
 }
---
+++ components/SearchBar.tsx
@@ -0,0 +1,27 @@
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { View, TextInput } from "react-native";
+
+interface Props {
+  placeholder: string;
+  value?: string;
+  onChangeText?: (text: string) => void;
+  onPress?: () => void;
+}
+
+const SearchBar = ({ placeholder, value, onChangeText, onPress }: Props) => {
+  return (
+    <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
+      <FontAwesome size={28} name='search' color="#AB8BFF" />
+      <TextInput
+        onPress={onPress}
+        placeholder={placeholder}
+        value={value}
+        onChangeText={onChangeText}
+        className="flex-1 ml-2 text-white"
+        placeholderTextColor="#A8B5DB"
+      />
+    </View>
+  );
+};
+
+export default SearchBar;
